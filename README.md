4dc742efd270cb6b486b260f8978f64dbd5aac04
pepepica12 <almalic8112@gmail.com>
2026-01-21 02:51:52 -0600

    yes

---
+++ README.md
@@ -0,0 +1,2 @@
+# Telemetr-a-
+
---
+++ app.py
@@ -0,0 +1,16 @@
+	mport os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+@app.route('/')
+def index():
+    return "Backend Flask conectado a Railway 🚀"
+
+
+

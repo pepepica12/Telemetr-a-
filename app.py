b3d98461c0964b6935e7c216cac2eb4252950ed0
pepepica12 <almalic8112@gmail.com>
2026-01-22 21:46:58 -0600

    Preparación para deploy en Railway

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-	mport os
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data/telemetria.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -12,5 +13,6 @@ db = SQLAlchemy(app)
 def index():
     return "Backend Flask conectado a Railway 🚀"
 
-
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
 

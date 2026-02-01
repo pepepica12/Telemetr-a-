de2a11db19cc25583135d796388b0f8071bb9dff
pepepica12 <almalic8112@gmail.com>
2026-02-01 17:12:50 -0600

    Crear index.js

---
+++ providers/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+    telegram: require('./telegram'),
+    whatsapp: require('./whatsapp'),
+    nefosys: require('./nefosys'),
+    scheduler: require('./scheduler'),
+    logger: require('./logger'),
+    config: require('./config'),
+    types: require('./types')
+};

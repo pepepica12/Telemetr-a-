9d9780a9b17099f86f6d2d7bc9e7823e5965b18a
pepepica12 <almalic8112@gmail.com>
2026-02-01 17:18:47 -0600

    Actualizar index.js

--- providers/whatsapp/index.js
+++ providers/whatsapp/index.js
@@ -1,4 +1,12 @@
+const config = require('./config');
+const { withRetry } = require('../retry');
+const monitor = require('../monitor');
+
 module.exports = {
-    apiKey: process.env.WHATSAPP_API_KEY || '',
-    providerUrl: 'https://api.whatsapp.com'
+    sendMessage: async (number, text) => {
+        return withRetry(async () => {
+            monitor.logEvent('whatsapp', 'sendMessage', { number, text });
+            console.log(`[WhatsApp] Enviando mensaje a ${number}: ${text}`);
+        }, { label: 'whatsapp.sendMessage' });
+    }
 };

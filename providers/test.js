5fac407cf6ba1c193f5444ab00c5ad2d166b4369
pepepica12 <almalic8112@gmail.com>
2026-02-01 17:10:56 -0600

    Actualizar test.js

--- providers/test.js
+++ providers/test.js
@@ -1,6 +1,17 @@
 const providers = require('./');
 
-providers.telegram.sendMessage('12345', 'Test Telegram');
-providers.whatsapp.sendMessage('999999', 'Test WhatsApp');
-providers.nefosys.sendEvent('test_event', { ok: true });
-providers.scheduler.run();
+(async () => {
+    console.log('--- TEST TELEGRAM ---');
+    await providers.telegram.sendMessage('12345', 'Test Telegram');
+
+    console.log('--- TEST WHATSAPP ---');
+    await providers.whatsapp.sendMessage('999999', 'Test WhatsApp');
+
+    console.log('--- TEST NEFOSYS ---');
+    await providers.nefosys.sendEvent('test_event', { ok: true });
+
+    console.log('--- TEST SCHEDULER ---');
+    providers.scheduler.run();
+
+    console.log('--- TEST COMPLETADO ---');
+})();
